198d9b49a14745ed6155ede1944de3c4dccb55f6
Jasper Deflander <jasper.deflander@hotmail.com>
2020-11-30 11:18:54 +0100

    Compress using content type instead of content encoding

--- elimity_insights_client.py
+++ elimity_insights_client.py
@@ -78,10 +78,7 @@ class Client:
         self._post(body, "custom-connector-domain-graphs")
 
     def _post(self, body: Any, path: str) -> None:
-        additional_headers = {
-            "Content-Encoding": "deflate",
-            "Content-Type": "application/json",
-        }
+        additional_headers = {"Content-Type": "application/prs.deflate-json"}
         data = _encode(body)
         self._request(additional_headers, data, "POST", path)
 
