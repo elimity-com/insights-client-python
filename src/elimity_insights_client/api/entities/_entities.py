1d08b84f8d8d97bbc352f4049ad0a2e81cd9d063
Maarten Decat <maarten.decat@gmail.com>
2023-07-04 16:56:20 +0200

    ran black

--- src/elimity_insights_client/api/entities/_entities.py
+++ src/elimity_insights_client/api/entities/_entities.py
@@ -10,7 +10,11 @@ from elimity_insights_client.api.entities._parse_query_results_page import (
 from elimity_insights_client.api.entities._query import query
 
 
-def entities(config: Config, entity_type: EntityType, linked_source_ids: Optional[Set[int]] = None) -> List[Entity]:
+def entities(
+    config: Config,
+    entity_type: EntityType,
+    linked_source_ids: Optional[Set[int]] = None,
+) -> List[Entity]:
     """
     List all entities of the given entity type from the given source.
 
@@ -19,7 +23,11 @@ def entities(config: Config, entity_type: EntityType, linked_source_ids: Optiona
     """
 
     sos = sources(config)
-    sources_to_use = [source for source in sos if linked_source_ids is None or source.id in linked_source_ids]
+    sources_to_use = [
+        source
+        for source in sos
+        if linked_source_ids is None or source.id in linked_source_ids
+    ]
     schemas = {source.id: source.domain_graph_schema for source in sources_to_use}
     que = query(entity_type, schemas)
     queries = [que]
